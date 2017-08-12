76f909c7e9b178050501e972517914b0f1fa4a17
Brandon Hong <hong.brandon@gmail.com>
2017-08-12 02:04:12 -0700

    add basic readme info, first rev of generate-seed

---
+++ README.md
@@ -0,0 +1,7 @@
+Run the following command to generate a random 81 character IOTA friendly seed.
+
+	python generate-seed.py
+
+Dependenies:
+	Python 2.7
+	
---
+++ generate-seed.py
@@ -0,0 +1,41 @@
+from random import SystemRandom
+import random, time, string
+
+MSG_move_cursor = '''Move your mouse for a few seconds to ensure randomness.
+Press enter when done.'''
+
+NUM_SEEDS = 20
+
+def prompt_user_for_randomness():
+    raw_input(MSG_move_cursor)
+
+def generate_seed():
+    seed_length = 81
+    valid_chars = string.ascii_uppercase + '9'
+    
+    def generate_entropy_rn():
+        sysrng = SystemRandom()
+        return ''.join([sysrng.choice(valid_chars) for i in range(seed_length)])
+
+    def generate_timestamp_rn(entropy_rn):
+        current_time = time.time()
+        random.seed(current_time)
+        timestamp_rn = ''
+        for i in range(seed_length):
+            timestamp_random_char = random.choice(valid_chars)
+            chars = (timestamp_random_char, entropy_rn[i])
+            timestamp_rn = timestamp_rn + random.choice(chars)
+        assert(len(timestamp_rn) == 81)
+        return timestamp_rn
+
+    entropy_rn = generate_entropy_rn()
+    return generate_timestamp_rn(entropy_rn)
+
+def main():
+    prompt_user_for_randomness()
+   
+    for i in range(NUM_SEEDS):
+        print 'Seed #%d'%(i+1)
+        print generate_seed()
+
+main()
